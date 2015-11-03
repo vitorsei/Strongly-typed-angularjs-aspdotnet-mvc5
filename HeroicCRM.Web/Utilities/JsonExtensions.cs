278974aff6fe29389b6a189c3be9738426fb9a84
vitorsei <seiji.vitor@gmail.com>
2015-11-03 13:50:19 -0200

    Creation a custom ActionResult

---
+++ HeroicCRM.Web/Utilities/JsonExtensions.cs
@@ -0,0 +1,21 @@
+﻿﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace HeroicCRM.Web.Utilities
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson<T>(this T obj, bool includeNull = true)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Converters = new JsonConverter[] { new StringEnumConverter() },
+                NullValueHandling = includeNull ? NullValueHandling.Include : NullValueHandling.Ignore
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
