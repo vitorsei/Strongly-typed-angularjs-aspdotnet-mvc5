306035ff8dc1c45d540df0a93f70fc5749ca26f1
vitorsei <seiji.vitor@gmail.com>
2015-11-04 00:22:24 -0200

    Strongly-typed AngularJS Bindings

--- HeroicCRM.Web/Controllers/CustomerController.cs
+++ HeroicCRM.Web/Controllers/CustomerController.cs
@@ -8,50 +8,50 @@ using HeroicCRM.Web.Models;
 
 namespace HeroicCRM.Web.Controllers
 {
-	public class CustomerController : HeroicCRMControllerBase
-	{
-		private readonly AppDbContext _context;
+    public class CustomerController : HeroicCRMControllerBase
+    {
+        private readonly AppDbContext _context;
 
-		public CustomerController(AppDbContext context)
-		{
-			_context = context;
-		}
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-		public ActionResult Index()
-		{
-			return View();
-		}
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public JsonResult All()
-		{
-			var customerModels = _context.Customers
-				.OrderByDescending(x => x.CreateDate)
-				.Project().To<CustomerViewModel>();
+        public JsonResult All()
+        {
+            var customerModels = _context.Customers
+                .OrderByDescending(x => x.CreateDate)
+                .Project().To<CustomerViewModel>();
 
-			return Json(customerModels.ToArray());
-		}
+            return BetterJson(customerModels.ToArray());
+        }
 
-		public JsonResult Add(AddCustomerForm form)
-		{
-			var customer = Mapper.Map<Customer>(form);
-			_context.Customers.Add(customer);
-			_context.SaveChanges();
+        public JsonResult Add(AddCustomerForm form)
+        {
+            var customer = Mapper.Map<Customer>(form);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
 
-			var model = Mapper.Map<CustomerViewModel>(customer);
-			return Json(model);
-		}
+            var model = Mapper.Map<CustomerViewModel>(customer);
+            return BetterJson(model);
+        }
 
-		public JsonResult Update(EditCustomerForm form)
-		{
-			var target = _context.Customers.Find(form.Id);
+        public JsonResult Update(EditCustomerForm form)
+        {
+            var target = _context.Customers.Find(form.Id);
 
-			Mapper.Map(form, target);
+            Mapper.Map(form, target);
 
-			_context.SaveChanges();
+            _context.SaveChanges();
 
-			var updatedCustomer = _context.Customers.Project().To<CustomerViewModel>().Single(x => x.Id == form.Id);
+            var updatedCustomer = _context.Customers.Project().To<CustomerViewModel>().Single(x => x.Id == form.Id);
 
-			return Json(updatedCustomer);
-		}
-	}
+            return BetterJson(updatedCustomer);
+        }
+    }
 }
