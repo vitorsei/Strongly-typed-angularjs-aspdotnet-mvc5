bde4967b96e79c0c63978ce56a2b2cd184744009
vitorsei <seiji.vitor@gmail.com>
2015-11-06 23:29:08 -0200

    Building the FormForModel Helper

--- HeroicCRM.Web/Helpers/AngularHelper.cs
+++ HeroicCRM.Web/Helpers/AngularHelper.cs
@@ -1,4 +1,9 @@
-﻿﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.Mvc;
+using HtmlTags;
 
 namespace HeroicCRM.Web.Helpers
 {
@@ -22,6 +27,45 @@ namespace HeroicCRM.Web.Helpers
         public AngularModelHelper<TModel> ModelFor(string expressionPrefix)
         {
             return new AngularModelHelper<TModel>(_htmlHelper, expressionPrefix);
+        }
+
+        public HtmlTag FormForModel(string expressionPrefix)
+        {
+            var modelHelper = ModelFor(expressionPrefix);
+
+            var formGroupForMethodGeneric = typeof(AngularModelHelper<TModel>)
+                .GetMethod("FormGroupFor");
+
+            var wrapperTag = new HtmlTag("div").NoTag();
+
+            foreach (var prop in typeof(TModel)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (prop.GetCustomAttributes().OfType<HiddenInputAttribute>().Any()) continue;
+
+                var formGroupForProp = formGroupForMethodGeneric
+                                    .MakeGenericMethod(prop.PropertyType);
+
+                var propertyLambda = MakeLambda(prop);
+
+                var formGroupTag = (HtmlTag)formGroupForProp.Invoke(modelHelper,
+                                    new[] { propertyLambda });
+
+                wrapperTag.Append(formGroupTag);
+            }
+
+            return wrapperTag;
+        }
+
+        //Constructs a lambda of the form x => x.PropName
+        private object MakeLambda(PropertyInfo prop)
+        {
+            var parameter = Expression.Parameter(typeof(TModel), "x");
+            var property = Expression.Property(parameter, prop);
+            var funcType = typeof(Func<,>).MakeGenericType(typeof(TModel), prop.PropertyType);
+
+            //x => x.PropName
+            return Expression.Lambda(funcType, property, parameter);
         }
     }
 }
