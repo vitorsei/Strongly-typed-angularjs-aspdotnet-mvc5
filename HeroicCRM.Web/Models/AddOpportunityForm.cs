a3143316ecf5dd9aa097e55d8cade829c325ea74
vitorsei <seiji.vitor@gmail.com>
2015-11-07 00:07:14 -0200

    Applying FormForModel Everywhere

--- HeroicCRM.Web/Models/AddOpportunityForm.cs
+++ HeroicCRM.Web/Models/AddOpportunityForm.cs
@@ -1,14 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using Heroic.AutoMapper;
 using HeroicCRM.Web.Core;
 
 namespace HeroicCRM.Web.Models
 {
-	public class AddOpportunityForm : IMapTo<Opportunity>
-	{
-		public int CustomerId { get; set; }
+    public class AddOpportunityForm : IMapTo<Opportunity>
+    {
+        [HiddenInput]
+        public int CustomerId { get; set; }
 
-		public string Title { get; set; }
+        [Required]
+        public string Title { get; set; }
 
-		public string Description { get; set; }
-	}
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+    }
 }
